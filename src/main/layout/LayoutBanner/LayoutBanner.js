e16bdc5bbb280a410f1f3df99e17abb1ad3a0107
kboom <gurgul.grzegorz@gmail.com>
2016-12-16 17:25:34 +0100

    Extracted theme

--- src/main/layout/LayoutBanner/LayoutBanner.js
+++ src/main/layout/LayoutBanner/LayoutBanner.js
@@ -1,7 +1,10 @@
 import React, {Component, PropTypes} from "react";
 import classnames from 'classnames/bind'
 import Slider from 'react-slick';
+import Banner from './Banner/Banner.js';
 import styles from './LayoutBanner.pcss'
+
+import image from '../../assets/background.jpg';
 
 const cx = classnames.bind(styles);
 
@@ -11,18 +14,30 @@ export default class LayoutBanner extends Component {
 
         const slickConfig = {
             dots: true,
-            infinite: true,
-            speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            centerMode: true,
+            slidesToScroll: 1,
+            adaptiveHeight: false,
+            variableWidth: false,
+            centerPadding: '0'
+        };
+
+        const coverStyle = {
+            width: '100%',
+            height: '400px',
         };
 
         return (
             <div className={cx('layout-banner')}>
                 <Slider className={cx('layout-banner__slider')} {...slickConfig}>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
+
+                    <div style={coverStyle}>
+                        <Banner img={image} />
+                    </div>
+                    <div style={coverStyle}>
+                        <Banner img={image}/>
+                    </div>
+
                 </Slider>
             </div>
         );
