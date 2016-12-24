f5c9f01b037033b184963ff60f5d330c743da8bc
kboom <gurgul.grzegorz@gmail.com>
2016-12-24 21:44:45 +0100

    can sign in

--- src/main/core/store.js
+++ src/main/core/store.js
@@ -4,5 +4,6 @@ import reducers from "./reducers";
 
 export default createStore(
     reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
 );
