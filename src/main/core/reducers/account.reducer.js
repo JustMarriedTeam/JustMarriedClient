271d6f4b92f5d2865f18fa4c732e65b79041023d
kboom <gurgul.grzegorz@gmail.com>
2016-12-26 23:17:46 +0100

    Fixed authentication process - regardless of whether need to login our choose account first it works.

--- src/main/core/reducers/account.reducer.js
+++ src/main/core/reducers/account.reducer.js
@@ -1,12 +1,15 @@
 import { ACCOUNT_CHANGE_STATE, AUTHENTICATE_WITH_TOKEN } from '../actions/account.actions';
-import Account from '../models/account.model';
+import Account, { ACCOUNT_STATE } from '../models/account.model';
 
 export default function (account = new Account(), action) {
   switch (action.type) {
     case ACCOUNT_CHANGE_STATE:
       return account.set('state', action.state);
     case AUTHENTICATE_WITH_TOKEN:
-      return account.set('token', action.token);
+      return account.withMutations((state) => {
+        state.set('state', ACCOUNT_STATE.SIGNED_IN);
+        state.set('token', action.token);
+      });
     default:
       return account;
   }
