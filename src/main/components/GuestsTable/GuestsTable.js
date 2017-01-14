1efbab9d83604a7e608a9a2efd857ecae4ad54c3
kboom <gurgul.grzegorz@gmail.com>
2017-01-14 14:34:36 +0100

    adjusting table of guests

--- src/main/components/GuestsTable/GuestsTable.js
+++ src/main/components/GuestsTable/GuestsTable.js
@@ -10,6 +10,7 @@ import {
 } from 'material-ui/Table';
 import classNames from 'classnames/bind';
 import styles from './GuestsTable.pcss';
+import reduce from 'lodash/reduce';
 
 const cx = classNames.bind(styles);
 
@@ -25,28 +26,35 @@ export default class GuestsTable extends PureComponent {
           { id: 'b', firstName: 'Agata', lastName: 'Nowakiewicz' },
           { id: 'c', firstName: 'Django', lastName: 'Szynszyl' },
           { id: 'd', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'e', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'f', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'g', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'h', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'i', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'j', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'k', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'l', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'm', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'n', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'o', firstName: 'Java', lastName: 'Szynszyl' },
+          { id: 'p', firstName: 'Java', lastName: 'Szynszyl' },
         ],
       },
-      guestCursor: {
-        a: {
-          selected: false,
-        },
-        b: {
-          selected: false,
-        },
-        c: {
-          selected: false,
-        },
-        d: {
-          selected: false,
-        },
-      },
     };
+
+    this.state.guestCursor = reduce(this.state.guestRegistry.guests, (result, guest) => {
+      result[guest.id] = {
+        isSelected: false,
+      };
+      return result;
+    }, {});
   }
 
   render() {
     return (
       <Table
+        height={'300px'}
+        fixedHeader={true}
         selectable={this.state.isSelectable}
         multiSelectable={this.state.isSelectable}
       >
@@ -58,7 +66,7 @@ export default class GuestsTable extends PureComponent {
         >
 
           <TableRow>
-            <TableHeaderColumn colSpan="2" tooltip="Super Header" style={{textAlign: 'center'}}>
+            <TableHeaderColumn colSpan="2" tooltip="Super Header" style={{ textAlign: 'center' }}>
               Super Header
             </TableHeaderColumn>
           </TableRow>
@@ -76,7 +84,7 @@ export default class GuestsTable extends PureComponent {
         >
 
           {this.state.guestRegistry.guests.map((guest) => (
-            <TableRow key={guest.id} selected={this.state.guestCursor[guest.id].selected}>
+            <TableRow key={guest.id} selected={this.state.guestCursor[guest.id].isSelected}>
               <TableRowColumn>{guest.firstName}</TableRowColumn>
               <TableRowColumn>{guest.lastName}</TableRowColumn>
             </TableRow>
