e610b9c0b6e3ac992b2bad4c3462c24edcd1645a
kboom <gurgul.grzegorz@gmail.com>
2017-03-22 20:06:40 +0100

    Cleaning up context menu after navigating out of wedding page.

--- src/main/core/actions/actionbar.actions.js
+++ src/main/core/actions/actionbar.actions.js
@@ -1,3 +1,5 @@
 export const DISPLAY_CONTEXT_MENU = 'DISPLAY_CONTEXT_MENU';
+export const DESTROY_CONTEXT_MENU = 'DESTROY_CONTEXT_MENU';
 
 export const displayContextMenu = (menu) => ({ type: DISPLAY_CONTEXT_MENU, menu });
+export const destroyContextMenu = (menu) => ({ type: DESTROY_CONTEXT_MENU, menu });
--- src/main/core/reducers/actionbar.reducer.js
+++ src/main/core/reducers/actionbar.reducer.js
@@ -1,10 +1,13 @@
-import { DISPLAY_CONTEXT_MENU } from '../actions/actionbar.actions';
+import React from 'react';
+import { DISPLAY_CONTEXT_MENU, DESTROY_CONTEXT_MENU } from '../actions/actionbar.actions';
 import ActionBar from '../models/actionbar.model';
 
 export default function (actionBar = new ActionBar(), action) {
   switch (action.type) {
     case DISPLAY_CONTEXT_MENU:
       return actionBar.set('menu', action.menu);
+    case DESTROY_CONTEXT_MENU:
+      return actionBar.set('menu', <div />);
     default:
       return actionBar;
   }
