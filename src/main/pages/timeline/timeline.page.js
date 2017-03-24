e5d93c19d19bf73c78bde15952011790d6c7a461
kboom <gurgul.grzegorz@gmail.com>
2017-03-24 10:20:02 +0100

    Added timeline

---
+++ src/main/pages/timeline/timeline.page.js
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+import Layout from '../../layout/Layout';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as allTasksActions from '../../core/actions/task.actions';
+import * as allModalActions from '../../core/actions/modal.actions';
+import * as actionBarActions from '../../core/actions/actionbar.actions';
+import * as allSelectionActions from '../../core/actions/selection.actions';
+import { selectTasks } from '../../core/selectors/tasks.selector';
+import Immutable from 'immutable';
+
+
+class TasksPage extends Component {
+
+  static propTypes = {
+    /**
+     * Set internally via connect.
+     */
+    tasks: PropTypes.instanceOf(Immutable.List).isRequired,
+    tasksActions: PropTypes.object.isRequired,
+    actionBarActions: PropTypes.object.isRequired,
+    selectionActions: PropTypes.object.isRequired,
+    modalActions: PropTypes.object.isRequired,
+  };
+
+  componentWillMount = () => this.props.tasksActions.fetchTasks();
+
+  render() {
+    return (
+      <Layout>
+        tasks
+      </Layout>
+    );
+  }
+
+}
+
+export default connect((state) => ({
+  tasks: selectTasks(state),
+}), (dispatch) => ({
+  actionBarActions: bindActionCreators(actionBarActions, dispatch),
+  tasksActions: bindActionCreators(allTasksActions, dispatch),
+  selectionActions: bindActionCreators(allSelectionActions, dispatch),
+  modalActions: bindActionCreators(allModalActions, dispatch),
+}))(TasksPage);
+
