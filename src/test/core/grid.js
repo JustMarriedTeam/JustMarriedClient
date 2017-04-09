817efc4704760791871882d9f3a4bf754754b2e3
kboom <gurgul.grzegorz@gmail.com>
2017-04-09 21:26:25 +0200

    Fixed grid tests

--- src/test/core/grid.js
+++ src/test/core/grid.js
@@ -6,22 +6,20 @@ describe('grid', () => {
   it('creates breakpoints', () => {
     const breakpoints = createGridBreakpoints();
     expect(breakpoints).to.be.eql({
-      lg: 1200,
-      md: 996,
-      sm: 768,
-      xs: 420,
-      xxs: 320,
+      lg: 1199,
+      md: 995,
+      sm: 767,
+      xs: 419,
     });
   });
 
   it('creates grid cols', () => {
     const cols = createGridCols();
     expect(cols).to.be.eql({
-      lg: 6,
-      md: 4,
-      sm: 3,
-      xs: 2,
-      xxs: 1,
+      lg: 4,
+      md: 3,
+      sm: 2,
+      xs: 1,
     });
   });
 
@@ -60,15 +58,15 @@ describe('grid', () => {
         i: '5',
         w: 1,
         h: 1,
-        x: 4,
-        y: 0,
+        x: 0,
+        y: 1,
       },
       {
         i: '6',
         w: 1,
         h: 1,
-        x: 5,
-        y: 0,
+        x: 1,
+        y: 1,
       },
     ]);
   });
@@ -101,21 +99,21 @@ describe('grid', () => {
         i: '4',
         w: 1,
         h: 1,
-        x: 3,
-        y: 0,
+        x: 0,
+        y: 1,
       },
       {
         i: '5',
         w: 1,
         h: 1,
-        x: 0,
+        x: 1,
         y: 1,
       },
       {
         i: '6',
         w: 1,
         h: 1,
-        x: 1,
+        x: 2,
         y: 1,
       },
     ]);
@@ -142,29 +140,29 @@ describe('grid', () => {
         i: '3',
         w: 1,
         h: 1,
-        x: 2,
-        y: 0,
-      },
-      {
-        i: '4',
-        w: 1,
-        h: 1,
         x: 0,
         y: 1,
       },
       {
-        i: '5',
+        i: '4',
         w: 1,
         h: 1,
         x: 1,
         y: 1,
       },
       {
+        i: '5',
+        w: 1,
+        h: 1,
+        x: 0,
+        y: 2,
+      },
+      {
         i: '6',
         w: 1,
         h: 1,
-        x: 2,
-        y: 1,
+        x: 1,
+        y: 2,
       },
     ]);
   });
@@ -172,54 +170,6 @@ describe('grid', () => {
   it('creates xs layout', () => {
     const layouts = createLayouts([1, 2, 3, 4, 5, 6]);
     expect(layouts.xs).to.be.eql([
-      {
-        i: '1',
-        w: 1,
-        h: 1,
-        x: 0,
-        y: 0,
-      },
-      {
-        i: '2',
-        w: 1,
-        h: 1,
-        x: 1,
-        y: 0,
-      },
-      {
-        i: '3',
-        w: 1,
-        h: 1,
-        x: 0,
-        y: 1,
-      },
-      {
-        i: '4',
-        w: 1,
-        h: 1,
-        x: 1,
-        y: 1,
-      },
-      {
-        i: '5',
-        w: 1,
-        h: 1,
-        x: 0,
-        y: 2,
-      },
-      {
-        i: '6',
-        w: 1,
-        h: 1,
-        x: 1,
-        y: 2,
-      },
-    ]);
-  });
-
-  it('creates xxs layout', () => {
-    const layouts = createLayouts([1, 2, 3, 4, 5, 6]);
-    expect(layouts.xxs).to.be.eql([
       {
         i: '1',
         w: 1,
