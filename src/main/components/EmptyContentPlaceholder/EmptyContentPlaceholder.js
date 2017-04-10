b7583e152e2944db2794070c33990f0e577a9b5b
kboom <gurgul.grzegorz@gmail.com>
2017-04-10 20:56:46 +0200

    No content placeholder

--- src/main/components/EmptyContentPlaceholder/EmptyContentPlaceholder.js
+++ src/main/components/EmptyContentPlaceholder/EmptyContentPlaceholder.js
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
-import AssignmentIcon from 'material-ui/svg-icons/action/assignment';
+import CreateIcon from 'material-ui/svg-icons/content/create';
 import ContentPlaceholder from '../ContentPlaceholder';
 
 export default class EmptyContentPlaceholder extends PureComponent {
 
   render() {
     return (
-      <ContentPlaceholder icon={<AssignmentIcon />}>
+      <ContentPlaceholder icon={<CreateIcon />}>
         <h3>Nothing here yet!</h3>
         <p>{this.props.children}</p>
       </ContentPlaceholder>
