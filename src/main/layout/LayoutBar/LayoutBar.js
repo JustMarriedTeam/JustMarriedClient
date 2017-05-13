6dd08bf8d322d7f75b3463bc10d32f7142af5371
Marta\Martaa <martaawronska@gmail.com>
2017-05-13 18:20:58 +0200

    home

--- src/main/layout/LayoutBar/LayoutBar.js
+++ src/main/layout/LayoutBar/LayoutBar.js
@@ -3,10 +3,34 @@ import AppBar from 'material-ui/AppBar';
 import * as actionBarActions from '../../core/actions/actionbar.actions';
 import { connect } from 'react-redux';
 import classNames from 'classnames/bind';
+import Scroll from 'react-scroll';
 import styles from './LayoutBar.pcss';
 import Account from '../../core/models/account.model';
+import $ from 'jquery'; 
 
 const cx = classNames.bind(styles);
+const ScrollToElement = Scroll.Element;
+const scroller = Scroll.scroller;
+
+let myStyle = {
+    background: 'rgba(229,120,120,0.7)',
+    position: 'fixed',
+    height: '60px',
+     
+};
+$(window).scroll(function() {
+  if($(window).scrollTop() > 0 ) {
+            
+    $(".scrolling").css('height', '45px');
+    $(".scrolling-menu span").css('font-size', '16px');
+
+    }else {
+     
+   $(".scrolling").css('height', '60px');
+   $(".scrolling-menu span").css('font-size', '20px');
+        
+  }  
+});
 
 class LayoutBar extends PureComponent {
 
@@ -15,18 +39,42 @@ class LayoutBar extends PureComponent {
     onMenuAction: PropTypes.func.isRequired,
     account: PropTypes.instanceOf(Account).isRequired,
   };
+  handleStart = () => {
+    scroller.scrollTo('start', {
+      duration: 1000,
+      delay: 100,
+      smooth: true,
+    });
+  };
+   handleMore = () => {
+    scroller.scrollTo('more', {
+      duration: 1000,
+      delay: 100,
+      smooth: true,
+    });
+  };
 
   render() {
     const { actionBarMenu, onMenuAction, account } = this.props;
     return (
       <AppBar
-        className={cx('layout-bar')}
+       style={myStyle}
+        className={[cx('layout-bar') , 'scrolling'].join(' ')} 
         onLeftIconButtonTouchTap={onMenuAction}
         iconElementRight={actionBarMenu}
         showMenuIconButton={account.isSignedIn()}
-        title="JustMarried"
+        title=""
         zDepth={0}
-      />
+        
+      >
+      <div className={[cx('layout-bar-menu') , 'scrolling-menu'].join(' ')} >
+          <span onClick={this.handleStart}>start</span>
+          <span onClick={this.handleMore}>dowiedz się więcej</span>
+          <span>działaj</span>
+
+      </div>
+
+      </AppBar>
     );
   }
 
@@ -36,3 +84,4 @@ export default connect((state) => ({
   actionBarMenu: state.actionBar.toJS().menu,
   account: state.account,
 }), actionBarActions)(LayoutBar);
+
--- src/main/pages/home/home.page.js
+++ src/main/pages/home/home.page.js
@@ -10,20 +10,59 @@ import Spacer from '../../components/Spacer';
 import ContentSection from '../../components/ContentSection';
 import ResponsiveBox from '../../components/ResponsiveBox';
 import ParallaxContent from '../../components/ParallaxContent';
+import MenuItem from 'material-ui/MenuItem';
+import FontIcon from 'material-ui/FontIcon';
 import Carousel from '../../components/Carousel';
 import CarouselItem from '../../components/Carousel/CarouselItem';
 import Scroll from 'react-scroll';
 import styles from './home.page.pcss';
 import whyNeedUs from '../../assets/whyneedus.jpg';
-import registrationBg from '../../assets/registrationbg.jpg';
+import photorings from '../../assets/photorings.jpg';
+import registrationBg from '../../assets/slide3.jpg';
 import { connect } from 'react-redux';
 import * as allAccountActions from '../../core/actions/account.actions';
 import Account from '../../core/models/account.model';
 import ConditionalRenderer from '../../utils/ConditionalRenderer';
-
+import { Parallax, Background } from 'react-parallax';
+import $ from 'jquery'; 
 const cx = classnames.bind(styles);
 const ScrollToElement = Scroll.Element;
 const scroller = Scroll.scroller;
+
+let h4Style = {
+     
+    color: '#e57878',
+    fontWeight: 300,
+    textAlign: 'center',
+};
+let h5Style = {
+     
+    color: '#e57878',
+    fontSize: '15px',
+    fontWeight: 300,
+    marginBottom: '20px',
+};
+let buttonStyle = {
+     
+    backgroundColor: '#e57878',
+    color: 'white',
+    padding: '5px',
+    height: 'auto',
+    margin: '0 10px',
+    boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.12), 0px 1px 4px rgba(0, 0, 0, 0.12)',
+     
+};
+ let buttonStyle2 = {
+
+    backgroundColor: '#6ed1d0',
+    color: 'white',
+    padding: '5px',
+    height: 'auto',
+    margin: '0 10px',
+    boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.12), 0px 1px 4px rgba(0, 0, 0, 0.12)',
+
+     
+};
 
 class HomePage extends React.Component {
 
@@ -58,39 +97,78 @@ class HomePage extends React.Component {
 
 
   render() {
+
+    
     function LoginPane(props) {
       if (props.isVisible) {
         return <LoginForm />;
       }
       return null;
     }
-
-
+    var settings = {
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        slidesToShow: 1,
+          slidesToScroll: 1,
+        // draggable: false,
+        fade: true,
+        cssEase: 'linear',
+        speed: 1000
+    };
+    
     return (
       <Layout>
+
+      <div className={'top-slider'}>
+      
+          <Carousel {...settings} style={{
+                height: '600px',
+              }}>
+              <CarouselItem key={1} img={'http://martaw.esy.es/images/slide1.jpg'}>
+                 
+              </CarouselItem>
+              <CarouselItem key={2} img={'http://martaw.esy.es/images/slide2.jpg'}>
+                 
+              </CarouselItem>
+               <CarouselItem key={3} img={'http://martaw.esy.es/images/slide3.jpg'}>
+                 
+              </CarouselItem>
+            </Carousel>
+      
+       </div>
+       <ScrollToElement name="start">
         <div className={cx('home__banner')}>
           <div className={cx('home__banner-content')}>
-            <div className={cx('home__logo')} />
+            
             <div className={cx('home__slogan')}>
 
-              <h1>Just Married</h1>
-              <h4>Let's plan your wedding!</h4>
+              <h1></h1>
+              <div className={cx('menu__top')}>
+              
+              </div>
+              <h2 style={h4Style}>Pomożemy Ci zaplanować wymarzone wesele!</h2>
+              <h5 style={h5Style}>Doskonale wiemy, jak wielkim wysiłkiem dla młodej pary jest organizacja wesela. Dlatego przedstawiamy Wam **bezpłatne** narzędzie, które poprowadzi Was przez wszystkie etapy tego procesu, czuwając nad tym, aby wszystko przebiegało zgodnie z planem. Od teraz możecie nazywać go Waszym własnym *Kreatorem Weselnym*.</h5>
 
               <ConditionalRenderer show={!this.props.account.isSignedIn()}>
                 <div>
-                  <RaisedButton
-                    label="Start here"
-                    secondary
+                  <FlatButton
+                    label="Rejestracja"
+                    
+                    style={buttonStyle}
                     onClick={this.handleStart}
                   />
 
                   <FlatButton
                     onClick={this.toggleLoginForm}
                     disabled={this.state.loginForm.shown}
-                    label="or sign in"
+                     
+                    style={buttonStyle2}
+                    label="Logowanie"
                     href={'#continue'}
                   />
                 </div>
+                 
               </ConditionalRenderer>
 
             </div>
@@ -101,34 +179,24 @@ class HomePage extends React.Component {
 
           </div>
         </div>
-
+        </ScrollToElement>
         <LayoutContainer>
-
-          <ParallaxContent img={whyNeedUs}>
-            <h2
-              style={{
-                padding: '120px 0',
-                textAlign: 'center',
-              }}
-            >Want to do this all by yourself?</h2>
-          </ParallaxContent>
-
-          <ContentSection alternate header={<h2>You don't have to! Let us help you.</h2>}>
-
-            <Carousel
-              style={{
-                height: '600px',
-              }}
-            >
-              <CarouselItem key={1} img={'https://placeholdit.imgix.net/~text?txtsize=90&txt=960%C3%97500&w=960&h=500'}>
-                abc
-              </CarouselItem>
-              <CarouselItem key={2} img={'https://placeholdit.imgix.net/~text?txtsize=90&txt=960%C3%97500&w=960&h=500'}>
-                def
-              </CarouselItem>
-            </Carousel>
-
-          </ContentSection>
+        <Parallax strength={300}>
+          <Background>
+            <img src="http://martaw.esy.es/images/work.jpg"/>
+            <div style={{
+               width: 800, 
+               height: 300, 
+               backgroundColor: '#fff'
+              }}></div>
+            
+          </Background>
+          <h1 style={{color : "#6ed1d0", textAlign: 'center', padding: "130px 0",}}>Chesz zrobić wszystko sam? </h1>
+        </Parallax>
+      <ScrollToElement name="more">
+      <div className={cx('home__banner')}>
+        <h2 style={h4Style}>Nie musisz, przedstawiamy Ci nasze rozwiązanie!</h2>
+      </div>
 
           <ConditionalRenderer show={!this.props.account.isSignedIn()}>
             <ParallaxContent img={registrationBg}>
@@ -147,7 +215,7 @@ class HomePage extends React.Component {
 
             </ParallaxContent>
           </ConditionalRenderer>
-
+          </ScrollToElement>
         </LayoutContainer>
 
 
@@ -161,3 +229,5 @@ class HomePage extends React.Component {
 export default connect((state) => ({
   account: state.account,
 }), allAccountActions)(HomePage);
+
+
