61144673861c1820067c29d1e756e7777e0c759e
kboom <gurgul.grzegorz@gmail.com>
2017-05-21 21:22:02 +0200

    Running in docker cloud

--- do.package.sh
+++ do.package.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-COMPONENT_NAME=jmclient \
- SERVER_API_URL=server.justmarried.5bbcd122.svc.dockerapp.io:2701 \
- bash -c "./JustMarriedTools/build.template.sh"
+COMPONENT_NAME=jmclient bash -c "./JustMarriedTools/build.template.sh"
