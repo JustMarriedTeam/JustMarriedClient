37d97cb73f1f1316e421fbeee081540e0ae3630a
kboom <gurgul.grzegorz@gmail.com>
2017-06-05 21:59:50 +0200

    Styling...

--- src/main/pages/home/home.page.js
+++ src/main/pages/home/home.page.js
@@ -1,23 +1,23 @@
-import React, {PropTypes} from "react";
-import Layout from "../../layout/Layout";
-import classnames from "classnames/bind";
-import LayoutContainer from "../../layout/LayoutContainer";
-import LoginForm from "../../components/LoginForm";
-import RegistrationForm from "../../components/RegistrationForm";
-import RaisedButton from "material-ui/RaisedButton";
-import Spacer from "../../components/Spacer";
-import ResponsiveBox from "../../components/ResponsiveBox";
-import ParallaxContent from "../../components/ParallaxContent";
-import Carousel from "../../components/Carousel";
-import CarouselItem from "../../components/Carousel/CarouselItem";
-import Scroll from "react-scroll";
-import styles from "./home.page.pcss";
-import registrationBg from "../../assets/slide3.jpg";
-import {connect} from "react-redux";
-import * as allAccountActions from "../../core/actions/account.actions";
-import Account from "../../core/models/account.model";
-import ConditionalRenderer from "../../utils/ConditionalRenderer";
-import {Background, Parallax} from "react-parallax";
+import React, { PropTypes } from 'react';
+import Layout from '../../layout/Layout';
+import classnames from 'classnames/bind';
+import LayoutContainer from '../../layout/LayoutContainer';
+import LoginForm from '../../components/LoginForm';
+import RegistrationForm from '../../components/RegistrationForm';
+import RaisedButton from 'material-ui/RaisedButton';
+import Spacer from '../../components/Spacer';
+import ResponsiveBox from '../../components/ResponsiveBox';
+import ParallaxContent from '../../components/ParallaxContent';
+import Carousel from '../../components/Carousel';
+import CarouselItem from '../../components/Carousel/CarouselItem';
+import Scroll from 'react-scroll';
+import styles from './home.page.pcss';
+import registrationBg from '../../assets/slide3.jpg';
+import { connect } from 'react-redux';
+import * as allAccountActions from '../../core/actions/account.actions';
+import Account from '../../core/models/account.model';
+import ConditionalRenderer from '../../utils/ConditionalRenderer';
+import { Background, Parallax } from 'react-parallax';
 import ContentSection from '../../components/ContentSection';
 import HorizontalButtonPanel from '../../components/HorizontalButtonPanel';
 import FeatureBoxContainer from '../../components/FeatureBoxContainer';
@@ -26,28 +26,6 @@ import FeatureBox from '../../components/FeatureBox';
 const cx = classnames.bind(styles);
 const ScrollToElement = Scroll.Element;
 const scroller = Scroll.scroller;
-
-let buttonStyle = {
-
-  backgroundColor: '#e57878',
-  color: 'white',
-  padding: '5px',
-  height: 'auto',
-  margin: '0 10px',
-  boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.12), 0px 1px 4px rgba(0, 0, 0, 0.12)',
-
-};
-let buttonStyle2 = {
-
-  backgroundColor: '#6ed1d0',
-  color: 'white',
-  padding: '5px',
-  height: 'auto',
-  margin: '0 10px',
-  boxShadow: '0px 1px 6px rgba(0, 0, 0, 0.12), 0px 1px 4px rgba(0, 0, 0, 0.12)',
-
-
-};
 
 class HomePage extends React.Component {
 
@@ -82,8 +60,6 @@ class HomePage extends React.Component {
 
 
   render() {
-
-
     function LoginPane(props) {
       if (props.isVisible) {
         return <LoginForm />;
@@ -97,24 +73,25 @@ class HomePage extends React.Component {
         <div className={'top-slider'}>
 
           <Carousel
+            className={cx('home__banner')}
             {...{
               autoplay: true,
               autoplaySpeed: 5000,
               dots: true,
               fade: true,
+              pauseOnHover: true,
               cssEase: 'linear',
-              speed: 1000
-            }} style={{
-            height: '600px',
-          }}>
+              speed: 1000,
+            }}
+          >
             <CarouselItem key={1} img={'http://martaw.esy.es/images/slide1.jpg'}>
-
+             <h1>Let's plan your wedding</h1>
             </CarouselItem>
             <CarouselItem key={2} img={'http://martaw.esy.es/images/slide2.jpg'}>
-
+              <h1>Let's plan your wedding 2</h1>
             </CarouselItem>
             <CarouselItem key={3} img={'http://martaw.esy.es/images/slide3.jpg'}>
-
+              <h1>Let's plan your wedding 3</h1>
             </CarouselItem>
           </Carousel>
 
@@ -127,48 +104,47 @@ class HomePage extends React.Component {
 
             <ContentSection centered>
 
-                    <h1>Pomożemy Ci zaplanować wymarzone wesele!</h1>
-                    <p>Doskonale wiemy, jak wielkim wysiłkiem dla młodej pary jest organizacja wesela. <br />
-                      Dlatego przedstawiamy Wam <b>bezpłatne</b> narzędzie, które poprowadzi Was przez wszystkie etapy tego
-                      procesu. <br /> Odpręż się bo, od teraz "Kreator weselny" ma wszystko pod kontrolą!</p>
+              <h1>Pomożemy Ci zaplanować wymarzone wesele!</h1>
+              <p>Doskonale wiemy, jak wielkim wysiłkiem dla młodej pary jest organizacja wesela. <br />
+                Dlatego przedstawiamy Wam <b>bezpłatne</b> narzędzie, które poprowadzi Was przez wszystkie etapy tego
+                procesu. <br /> Odpręż się bo, od teraz "Kreator weselny" ma wszystko pod kontrolą!</p>
 
-                    <ConditionalRenderer show={!this.props.account.isSignedIn()}>
+              <ConditionalRenderer show={!this.props.account.isSignedIn()}>
 
-                      <HorizontalButtonPanel buttons={[
-                        <RaisedButton
-                          label="Rejestracja"
-                          onClick={this.handleStart}
-                          primary
-                        />,
-                        <RaisedButton
-                          onClick={this.toggleLoginForm}
-                          disabled={this.state.loginForm.shown}
-                          label="Logowanie"
-                          href={'#continue'}
-                          secondary
-                        />
-                      ]}/>
+                <HorizontalButtonPanel
+                  buttons={[
+                    <RaisedButton
+                      label="Rejestracja"
+                      onClick={this.handleStart}
+                      primary
+                    />,
+                    <RaisedButton
+                      onClick={this.toggleLoginForm}
+                      disabled={this.state.loginForm.shown}
+                      label="Logowanie"
+                      href={'#continue'}
+                      secondary
+                    />,
+                  ]}
+                />
 
-                    </ConditionalRenderer>
+              </ConditionalRenderer>
 
-                  <Spacer />
+              <Spacer />
 
-                  <LoginPane isVisible={this.state.loginForm.shown}/>
+              <LoginPane isVisible={this.state.loginForm.shown} />
 
             </ContentSection>
 
           </ScrollToElement>
 
 
-
-
           <Parallax strength={300}>
             <Background>
-              <img src="http://martaw.esy.es/images/work.jpg"/>
+              <img src="http://martaw.esy.es/images/work.jpg" />
             </Background>
-            <h2 style={{textAlign: 'center', padding: "130px 0"}}>Na prawdę chcesz robic to sam?</h2>
+            <h2 style={{ textAlign: 'center', padding: '130px 0' }}>Na prawdę chcesz robic to sam?</h2>
           </Parallax>
-
 
 
           <ScrollToElement name="more">
@@ -207,7 +183,7 @@ class HomePage extends React.Component {
                   <FeatureBox
                     title="Manage guests"
                     icon="https://image.freepik.com/free-icon/calendar-page-of-day-25_318-58109.jpg"
-                  />
+                  />,
                 ]}
               />
 
@@ -244,5 +220,4 @@ class HomePage extends React.Component {
 export default connect((state) => ({
   account: state.account,
 }), allAccountActions)(HomePage);
-
 
